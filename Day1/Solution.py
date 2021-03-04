2ebd29368a055a76e41a7bf9093b27ce8ba39983
Diksha Dutt <69179939+dikshadutt08@users.noreply.github.com>
2021-03-05 01:42:46 +0530

    Create Solution.py

---
+++ Day1/Solution.py
@@ -0,0 +1,17 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Declare second integer, double, and String variables.
+num=int(input())
+num1=float(input())
+string=input()
+
+# Print the sum of both integer variables on a new line.
+print(num+i)
+
+# Print the sum of the double variables on a new line.
+print(num1+d)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s+string)
